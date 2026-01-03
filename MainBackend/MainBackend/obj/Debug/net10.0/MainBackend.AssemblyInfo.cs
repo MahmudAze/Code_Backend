70b61b3fb05f100ad94289b76999b8bc15da268c
MahmudAze <mahmudrahimli2006@gmail.com>
2026-01-03 16:02:14 +0400

    MainBackend: Added Slider page in Admin panel

--- MainBackend/MainBackend/obj/Debug/net10.0/MainBackend.AssemblyInfo.cs
+++ MainBackend/MainBackend/obj/Debug/net10.0/MainBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86adbaf1ef1e1a8434d051beeb2a285d847999d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d5c1e3e8b7cbf9dfed5aaf9e991cb0ba938bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
